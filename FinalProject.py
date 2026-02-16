84e568b6dba3c0f146a201e22f316b599714491d
Aleksey <ignissopitus@gmail.com>
2026-02-16 12:22:12 +0300

    Create FinalProject.py
    Initiation

---
+++ FinalProject.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+
+# Константы
+RANDOM_SEED = 42
+GENRE = ''
+DATES = []
+URL = ''
+
+# Парсер сайта
+
+def simple_parser(url):
+    pass
+
+df = simple_parser(URL)
+
+# Графики для кучи
+
+plt.plot()
+plt.show()
+
+# Базовая модель
